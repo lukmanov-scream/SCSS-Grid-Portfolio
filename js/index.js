738aee565494a6b7fe682465d6d486cfc492104c
Rodion Lukmanov <lukmanov.scream@gmail.com>
2023-02-06 23:17:10 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+function app() {
+	const buttons = document.querySelectorAll('.button')
+	const cards = document.querySelectorAll('.card')
+
+	function filter (category, items) {
+		items.forEach((item) => {
+			const isItemFiltered = !item.classList.contains(category)
+			const isShowAll = category.toLowerCase() === 'all'
+			if (isItemFiltered && !isShowAll) {
+				item.classList.add('anime')
+			} else {
+				item.classList.remove('hide')
+				item.classList.remove('anime')
+			}
+		})
+	}
+
+	buttons.forEach((button) => {
+		button.addEventListener('click', () => {
+			const currentCategory = button.dataset.filter
+			filter(currentCategory, cards)
+		})
+	}) 
+
+	cards.forEach((card) => {
+		card.ontransitionend = function () {
+			if(card.classList.contains('anime'))
+			card.classList.add('hide')
+		}
+	})
+}
+app()
